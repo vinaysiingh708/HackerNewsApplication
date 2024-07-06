b72594e007c7de2e0c9978d450afc40c936b45df
vinaysiingh708 <40298086+vinaysiingh708@users.noreply.github.com>
2024-07-06 12:29:15 +0530

    Update datatable.component.spec.ts

--- src/app/datatable/datatable.component.spec.ts
+++ src/app/datatable/datatable.component.spec.ts
@@ -1,15 +1,37 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import {  NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { By } from '@angular/platform-browser';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { DatatableComponent } from './datatable.component';
 
 describe('DatatableComponent', () => {
   let component: DatatableComponent;
   let fixture: ComponentFixture<DatatableComponent>;
 
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ DatatableComponent ],
+      imports: [
+        HttpClientTestingModule,
+        MatFormFieldModule,
+        MatTableModule,
+        MatPaginatorModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatTableModule,
+        MatPaginatorModule,
+        NoopAnimationsModule
+      
+      ]
+    })
+    .compileComponents();
+  });
+
   beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [DatatableComponent]
-    });
     fixture = TestBed.createComponent(DatatableComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -17,5 +39,18 @@ describe('DatatableComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should contain a mat-form-field with an input', () => {
+    const matFormField = fixture.debugElement.query(By.css('mat-form-field'));
+    expect(matFormField).not.toBeNull();
+
+    const input = matFormField.query(By.css('input'));
+    expect(input).not.toBeNull();
+  });
+
+  it('should display the data table', () => {
+    const table = fixture.debugElement.query(By.css('mat-table'));
+    expect(table).not.toBeNull();
   });
 });
